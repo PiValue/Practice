f753c40aa2a66c369be870e1370f3ec30f090a9e
PiValue <chethan.rudramuni@gmail.com>
2017-01-24 19:46:01 -0800

    Leetcode 206 - Reverse given linked list

--- src/com/leetcode/list/ReverseList206.java
+++ src/com/leetcode/list/ReverseList206.java
@@ -1,5 +1,19 @@
 package com.leetcode.list;
 
-public class ReverseList206 {
-    
+/**
+ * Reverse given linked list.
+ * 
+ * https://leetcode.com/problems/reverse-linked-list/
+ */
+public class ReverseList206<T> {
+    public ListNode<T> reverseList(ListNode<T> head) {
+        ListNode<T> prev = null;
+        while(head != null) {
+            ListNode<T> temp = head.next;
+            head.next = prev;
+            prev = head;
+            head = temp;
+        }
+        return prev;
+    }
 }
